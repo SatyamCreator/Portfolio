0808943ab6ebe17794bb60b70aa7c36a358e1804
Satyam Creator <105617640+SatyamCreator@users.noreply.github.com>
2024-03-05 18:24:28 +0530

    n

---
+++ style/style.js
@@ -0,0 +1,52 @@
+
+var navbar = document.querySelector('nav')
+
+window.onscroll = function () {
+
+  // pageYOffset or scrollY
+  if (window.pageYOffset > 0) {
+    navbar.classList.add('scrolled')
+  } else {
+    navbar.classList.remove('scrolled')
+  }
+}
+
+
+
+let a = 0;
+
+window.addEventListener('scroll', function() {
+    const oTop = document.getElementById('counter').getBoundingClientRect().top - window.innerHeight;
+
+    if (a === 0 && window.pageYOffset > oTop) {
+        const counterValues = document.querySelectorAll('.counter-value');
+
+        counterValues.forEach(function(element) {
+            const countTo = parseInt(element.getAttribute('data-count'));
+            const initialValue = parseInt(element.textContent);
+            let currentNum = initialValue;
+
+            const duration = 4000;
+            const startTime = performance.now();
+
+            function updateText() {
+                const timePassed = performance.now() - startTime;
+                const progress = Math.min(timePassed / duration, 1);
+
+                currentNum = initialValue + Math.floor((countTo - initialValue) * progress);
+                element.textContent = currentNum;
+
+                if (progress < 1) {
+                    requestAnimationFrame(updateText);
+                } else {
+                    element.textContent = countTo;
+                }
+            }
+
+            updateText();
+        });
+
+        a = 1;
+    }
+});
+
