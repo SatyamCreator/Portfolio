b3f5e6a445412c2abfafdaca5fdf65aca0b7c2d1
Satyam Creator <105617640+SatyamCreator@users.noreply.github.com>
2024-12-17 13:42:45 +0530

    n

---
+++ sites.js
@@ -0,0 +1,75 @@
+// data.js
+const sites = [
+  {
+    name: "Cedna Biotech",
+    image: "img/Clients/cednaimgmain.jpg",
+    description: "Cedna Biotech is a dynamic, innovative pharmaceutical company that cares about the health and well-being of people across the Globe.",
+    link: "https://www.cednabiotech.com/"
+  },
+  {
+    name: "Biotech Healthcare",
+    image: "img/Clients/biotech-healthcare.png",
+    description: "Biotech Healthcare is the global healthcare company that is engaged in manufacturing and exporting of Medical Equipment.",
+    link: "https://biotech-healthcare.com/"
+  },
+  {
+    name: "Historika Foundations",
+    image: "img/Clients/finelhistorika.png",
+    description: "Chandru Ramesh is an accomplished author, researcher, and speaker specializing in Indian history. He left his software engineering.",
+    link: "https://historika.org/"
+  },
+  {
+    name: "Accudel",
+    image: "img/Clients/accudel2.png",
+    description: "Accudel is a brain child of two industry veterans having worked with leading logistics companies with vast combined logistics experience.",
+    link: "https://www.accudelenterprises.com/"
+  },
+  {
+    name: "SFPL Formulations",
+    image: "img/Clients/sfpl.png",
+    description: "SFPL is a dedicated division for manufacturing & marketing the quality medicine, which is developed under world standard manufacturing units.",
+    link: "https://www.sfplformulations.com/"
+  },
+  {
+    name: "Advocate Pramod Kumar",
+    image: "img/Clients/advoPrmod.jpg",
+    description: "Adv. Pramod Kumar is among those few advocates who practice the law in the courts of law with high degree of legal, moral and ethical values.",
+    link: "https://advocatepramodkumar.in/"
+  },
+  {
+    name: "Apema Group",
+    image: "img/Clients/apema.png",
+    description: "Apema Entertainment Pvt. Ltd, an exclusive Entertainment & Media Public relations company based in Delhi. Since our founding in 2020.",
+    link: "http://apemagroup.in/"
+  },
+  {
+    name: "Aristo Gold",
+    image: "img/Clients/aristogold.jpeg",
+    description: "Do you want to get cash by selling your old gold? That too at today’s Gold Market Price, Connect to us without any alternate study.",
+    link: "https://aristogold.in/"
+  },
+  {
+    name: "Registration Guru",
+    image: "img/Clients/rg.png",
+    description: "We are one of the leading consultancy organizations with experienced professionals to provide quality services at reasonable cost.",
+    link: "https://registrationguru.in/"
+  },
+  {
+    name: "B.D. Intermediate College",
+    image: "img/Clients/bdcollage.png",
+    description: "Established in 1954, The B.D. Intermediate College Maniha Govindpur Jaunpur was the first school in the area surrounded by more than a hundred villages.",
+    link: "https://bdintercollege.in/"
+  },
+  {
+    name: "Arvore Healthcare",
+    image: "img/Clients/arvorehealthcare.png",
+    description: "Árvore Healthcare is the global healthcare company that is engaged in manufacturing and exporting of Medical Equipment, Hospital Furniture.",
+    link: "https://www.arvorehealthcare.com/"
+  },
+  {
+    name: "Better World Conference",
+    image: "img/Clients/bwc.png",
+    description: "The global sustainable development agenda focuses on realizing the ‘world we want’. The path to ‘world we want’ is a continuous evolutionary.",
+    link: "http://www.betterworldconference.com/"
+  }
+];
---
+++ skills.js
@@ -0,0 +1,15 @@
+// data.js
+const skills = [
+  { name: "Vs Code", image: "img/icons/vscode.svg"},
+  { name: "HTML 5", image: "img/icons/html5.svg"},
+  { name: "CSS 3", image: "img/icons/css.svg"},
+  { name: "Javascript", image: "img/icons/javascript.svg"},
+  { name: "Bootstrap", image: "img/icons/bootstrap.svg"},
+  { name: "Jquery", image: "img/icons/jquery.svg"},
+  { name: "Photoshop", image: "img/icons/photoshop.svg"},
+  { name: "Illustrator", image: "img/icons/illustator.svg"},
+  { name: "Canva", image: "img/icons/canva.svg"},
+  { name: "Sass", image: "img/icons/sass.svg"},
+  { name: "Filezilla", image: "img/icons/filezila.svg"},
+  { name: "Git Hub", image: "img/icons/github.svg"},
+];
